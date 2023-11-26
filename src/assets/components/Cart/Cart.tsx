7e3e02eae276f7689982027a73fcb7913bcceba3
huhhell <vika20020807@mail.ru>
2023-11-26 22:07:02 +0700

    style cart component

--- src/assets/components/Cart/Cart.tsx
+++ src/assets/components/Cart/Cart.tsx
@@ -1,5 +1,6 @@
 import closeImg from './images/close.svg';
 import {IProduct} from "../../data/products.ts";
+import './cart.scss';
 
 interface IProps {
     itemsInCart: IProduct[]
@@ -18,19 +19,21 @@ export default function Cart({itemsInCart}: IProps) {
         <div className="cart__list">
             {itemsInCart.map((i: IProduct) => {
                 return <div className='cart__item'>
-                    <img src={i.img} alt={i.alt} className="cart__item-img"/>
-                    <div className="cart__item-info">
-                        <h4 className="cart__item-title">{i.title}</h4>
-                        <p className="cart__item-count">Quantity {i.count}</p>
-                        <p className="cart__item-price">${i.price}</p>
+                    <div className="cart__item-product">
+                        <img src={i.img} alt={i.alt} className="cart__item-img"/>
+                        <div className="cart__item-info">
+                            <h4 className="cart__item-title">{i.title}</h4>
+                            <p className="cart__item-count">Quantity ({i.count})</p>
+                            <p className="cart__item-price">${i.price}</p>
+                        </div>
                     </div>
                     <button className="cart__item-delete">Remove</button>
                 </div>
             })}
         </div>
         <div className="cart__total">
-            <h3 className="cart__total-title"></h3>
-            <p className="cart__total-price"></p>
+            <h3 className="cart__total-title">Subtotal</h3>
+            <p className="cart__total-price">${itemsInCart.reduce((sum, i) => sum + (i.price * i.count), 0)}.00</p>
         </div>
         <textarea name="message" className="cart__message" placeholder='Gift Message'></textarea>
         <p className="cart__tip">
