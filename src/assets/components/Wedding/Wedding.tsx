d9f1f1fefb6defa6d822092a6a962cf509aa6f4b
huhhell <vika20020807@mail.ru>
2023-11-30 15:41:52 +0700

    wedding component

---
+++ src/assets/components/Wedding/Wedding.tsx
@@ -0,0 +1,11 @@
+import './wedding.scss'
+
+export default function Wedding() {
+
+    return <div className='wedding'>
+        <p className="wedding__subtitle">service</p>
+        <h3 className="wedding__title">Wedding & Event Decor</h3>
+        <p className="wedding__text">Let our team of expert florists and designers create stunning, on-trend floral décor for your special day. Trust us to bring your vision to life.</p>
+        <button className="wedding__inquire">Inquire Now</button>
+    </div>
+}
--- src/assets/pages/Main.tsx
+++ src/assets/pages/Main.tsx
@@ -2,6 +2,7 @@ import Intro from "../components/Intro/Intro.tsx";
 import AboutUs from "../components/AboutUs/AboutUs.tsx";
 import WhyUs from "../components/WhuUs/WhyUs.tsx";
 import Contact from "../components/Contact/Contact.tsx";
+import Wedding from "../components/Wedding/Wedding.tsx";
 
 export default function Main() {
     return <>
@@ -9,5 +10,6 @@ export default function Main() {
         <AboutUs />
         <WhyUs />
         <Contact />
+        <Wedding />
     </>
 }
