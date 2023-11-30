0a92d100ea2d53996bd7c84b3b715b8f25ae23b7
huhhell <vika20020807@mail.ru>
2023-11-30 13:21:41 +0700

    WhyUs component with styles and media

---
+++ src/assets/components/WhuUs/WhyUs.tsx
@@ -0,0 +1,45 @@
+import './whyUs.scss'
+
+export default function WhyUs() {
+
+    return <div className='why-us'>
+        <h3 className="why-us__title">Why choose us?</h3>
+        <div className="why-us__list">
+            <div className="why-us__list-item">
+                <h5 className="why-us__list-title">Stylish bouquets by florists</h5>
+                <p className="why-us__list-text">
+                    At our floral studio, our professional florists craft the most elegant
+                    and stylish bouquets using only the freshest and highest quality materials available. We stay
+                    up-to-date with the latest floral design trends and offer unique arrangements that are sure to
+                    impress. Let us brighten up your day with our stunning bouquets and same-day delivery service.
+                </p>
+            </div>
+            <div className="why-us__list-item">
+                <h5 className="why-us__list-title">On-time delivery</h5>
+                <p className="why-us__list-text">
+                    Never miss a moment with our on-time flower delivery service. Our
+                    couriers will deliver your bouquet personally, without boxes, to ensure it arrives in perfect
+                    condition. Trust us to deliver your thoughtful gift reliably.
+                </p>
+            </div>
+            <div className="why-us__list-item">
+                <h5 className="why-us__list-title">Safe payment</h5>
+                <p className="why-us__list-text">
+                    You can feel secure when placing an order with us, as we use
+                    industry-standard security measures to protect your payment information. Your transaction will be
+                    safe and hassle-free, so you can shop with confidence.
+                </p>
+            </div>
+            <div className="why-us__list-item">
+                <h5 className="why-us__list-title">Subscription by your needs</h5>
+                <p className="why-us__list-text">
+                    With our subscription service tailored to your specific needs, you can
+                    enjoy the convenience of having beautiful bouquets delivered straight to your door at regular
+                    intervals. Our flexible service is perfect for busy individuals or those who want to ensure they
+                    always have fresh flowers on hand. You'll save time and money with this hassle-free solution to your
+                    floral needs.
+                </p>
+            </div>
+        </div>
+    </div>
+}
--- src/assets/pages/Main.tsx
+++ src/assets/pages/Main.tsx
@@ -1,9 +1,13 @@
 import Intro from "../components/Intro/Intro.tsx";
 import AboutUs from "../components/AboutUs/AboutUs.tsx";
+import WhyUs from "../components/WhuUs/WhyUs.tsx";
+import Contact from "../components/Contact/Contact.tsx";
 
 export default function Main() {
     return <>
         <Intro />
         <AboutUs />
+        <WhyUs />
+        <Contact />
     </>
 }
