93b724b42d878f21ef2258b1905d86f22f30ae46
huhhell <vika20020807@mail.ru>
2024-04-09 13:10:14 +0700

    fix service block. Add missed borders.

--- src/assets/components/Service/Service.tsx
+++ src/assets/components/Service/Service.tsx
@@ -5,7 +5,9 @@ import './service.scss'
 export default function Service() {
     
     return <div className='service'>
-        <img src={mainImg} alt="girl with flowers" className="service__img"/>
+        <div className="service__img-cnt">
+            <img src={mainImg} alt="girl with flowers" className="service__img"/>
+        </div>
         <div className="service__info">
             <div className="service__info-container">
                 <p className="service__subtitle">service</p>
